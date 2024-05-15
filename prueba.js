40e324b7a4e231b2029e1f3ebef5f9e3d1dc160c
joseguraUADE <164243126+joseguraUADE@users.noreply.github.com>
2024-05-15 14:55:07 -0300

    Add files via upload
    ejercicios de js

---
+++ prueba.js
@@ -0,0 +1,41 @@
+// EJERCICIO 1
+/*
+var nombre = "Joaquin";
+var edad = 26;
+var ciudad = "Corrientes";
+
+var mensaje = "¡Bienvenido " + nombre + "! Tenes " + edad + " años y sos de " + ciudad;
+
+alert(mensaje);
+*/
+
+
+
+// EJERCICIO 2
+/*
+var nombre = prompt("Por favor, ingresa tu nombre:");
+var edad = prompt("Ingresa tu edad:");
+var ciudad = prompt("Ingresa tu ciudad:");
+
+var mensaje = "¡Bienvenido " + nombre + "! Tenes " + edad + " años y sos de " + ciudad;
+
+alert(mensaje);
+*/
+
+
+
+// EJERCICIO 3
+var nombre = prompt("Por favor, ingresa tu nombre:");
+var edad = prompt("Ingresa tu edad:");
+var ciudad = prompt("Ingresa tu ciudad:");
+
+var diasVividos = edad * 365; 
+
+var mensajeBienvenida = "¡Bienvenido, " + nombre + "! Tenes " + edad + " años y sos de " + ciudad;
+var mensajeDiasVividos = "Viviste aproximadamente " + diasVividos + " días desde que naciste.";
+
+alert(mensajeBienvenida);
+alert(mensajeDiasVividos);
+
+
+
